9bb12906388df05510c06a32d6d5231b39dc75dd
Botond Bugja <bugjabotond@gmail.com>
2025-12-18 14:58:32 +0100

    fdhsdfh

--- frontend/javascript/index5.js
+++ frontend/javascript/index5.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     fetchElemek();
     tablazatGeneralas();
+    document.getElementById('lekerdezesBtn').addEventListener('click', elemTablazat);
 });
 
 const getMethodFetch = (url) => {
@@ -35,26 +36,55 @@ const tablazatGeneralas = async () => {
         const tableSpan = document.getElementById("tableSpan");
         const table = document.createElement('table');
         const sor1 = document.createElement('tr');
-        const sor2 = document.createElement('tr');
 
-        console.log(Object.values(data.result[0]));
 
-        // for (let i = 0; i < Object.keys(data.result.length); i++) {
-        //     console.log(Object.keys(data.result[i]));
-        // }
+        Object.keys(data.result[0]).forEach(element => {
+            const th = document.createElement('th');
+            th.innerHTML = element;
+            sor1.appendChild(th);
+        });
+
+        table.appendChild(sor1);
 
         for (let i = 0; i < Object.keys(data.result).length; i++) {
-            console.log(Object.keys(data.result[i]));
-            // const th = document.createElement('th');
-            // th.innerHTML = Object.keys(data.result)[0];
-            // sor1.appendChild(th);
+            const tr = document.createElement('tr');
+            for (let j = 0; j < Object.values(data.result[i]).length; j++) {
+                const td = document.createElement('td');
+                td.innerHTML = Object.values(data.result[i])[j];
+                tr.appendChild(td)
+            }
+            table.appendChild(tr);
         }
+        
+        table.setAttribute("class", "table table-bordered text-center table-striped align-middle shadow-sm mt-3 w-50 mx-auto");
 
-        // for (let i = 0; i < Object.values(data.result).length; i++) {
-        //     const td = document.createElement('td');
-        //     td.innerHTML = Object.values(data.result)[i];
-        //     sor2.appendChild(td);
-        // }
+        tableSpan.replaceChildren(table);
+
+    } catch (error) {
+        console.error('Hiba történt: ', error);
+    }
+}
+
+const elemTablazat = async () => {
+    try {
+        const input = document.getElementById("elemTextbox");
+        const data = await getMethodFetch (`http://127.0.0.1:3000/api/getelem/${input.value}`);
+        const tableSpan = document.getElementById("elemTable");
+        const table = document.createElement('table');
+        const sor1 = document.createElement('tr');
+        const sor2 = document.createElement('tr');
+
+        Object.keys(data.result).forEach(element => {
+            const th = document.createElement('th');
+            th.innerHTML = element;
+            sor1.appendChild(th);
+        });
+
+        for (let i = 0; i < Object.values(data.result).length; i++) {
+            const td = document.createElement('td');
+            td.innerHTML = Object.values(data.result)[i];
+            sor2.appendChild(td);
+        }
 
         table.replaceChildren(sor1, sor2);
         
